574fe13e4278ba0fbb2a41f2f938ace1d513e08e
Rodrigo Morais <rodigu@users.noreply.github.com>
2020-08-23 20:21:01 -0300

    added dancin

---
+++ emoji.js
@@ -0,0 +1,17 @@
+class Emoji{
+  constructor(x_, y_, img_, name_, s_){
+    this.x = x_;
+    this.y = y_;
+    this.name = name_;
+    this.img = createImg(img_, "");
+    this.img.size(s_, 1.5*s_);
+    this.text = createElement('h1', this.name);
+  }
+  update(){
+    this.text.html(this.name);
+  }
+  movement(x_, y_){
+    this.text.position(x_, y_ - 50);
+    this.img.position(x_, y_);
+  }
+}
--- script.js
+++ script.js
@@ -1,24 +1,16 @@
+
 let vnt = false;
+var counter = 0;
 let phrase = "This sentence will write itself slowly";
-let counter;
-let move = true;
+var leon;
 function setup(){
   frameRate(50);
   canvas1 = createCanvas(100, 100);
+  input = createInput("Leon");
+  fbutton = createButton("Update text");
+  sizeslide = createSlider(0, 500, 50);
   paragraph = createElement('h1', "Current time: " + hour());
-  follow = createElement('h1', "&#129409;<br>i will follow u >:D");
-  fbutton = createButton("Following Lion");
-}
-
-function movedec(){
-  if (move){
-    move = false;
-    follow.html("&#129409;<br>i stop following*_*");
-  }
-  else{
-    move = true;
-    follow.html("&#129409;<br>i will follow u >:D");
-  }
+  leon = new Emoji(mouseX, mouseY, "resources/dancin.gif", "Leon");
 }
 
 // function mousePressed(){
@@ -29,9 +21,14 @@ function movedec(){
 // function mouseReleased(){
 //   move = true;
 // }
-
+function nameChange(){
+  leon.name = input.value();
+}
 function draw(){
-  fbutton.mousePressed(movedec);
+  leon.img.size(sizeslide.value(), 1.5*sizeslide.value());
+  leon.movement(mouseX + 10, mouseY + 100);
+  fbutton.mousePressed(nameChange);
+  leon.update();
   if (millis() >= 1000 && !vnt){
     vnt = true;
     counter = 0;
@@ -42,8 +39,6 @@ function draw(){
     counter++;
   }
   if (counter >= phrase.length && frameCount%100 == 0) vnt = false;
-  if (move) follow.position(mouseX + 8, mouseY + 110);
-  background(255);
   fill(230);
   ellipse(50, 50, 50, 50);
 }
