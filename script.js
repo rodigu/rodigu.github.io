bff92cfa6fcb220fe67118489722b2b99f2312b8
Rodrigo Morais <rodigu@users.noreply.github.com>
2020-08-23 13:40:01 -0300

    lion will spot following when mouse pressed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let vnt = false;
 let phrase = "This sentence will write itself slowly";
 let counter;
+let move = true;
 function setup(){
   frameRate(50);
   canvas1 = createCanvas(100, 100);
@@ -8,11 +9,18 @@ function setup(){
   follow = createElement('h1', "&#129409; <br> i will follow u *o*");
 }
 
+function mousePressed(){
+  console.log(follow.position(), '\n', mouseX, ' ', mouseY);
+  move = false;
+}
+function mouseReleased(){
+  move = true;
+}
+
 function draw(){
   if (millis() >= 1000 && !vnt){
-
     vnt = true;
-    counter = 0
+    counter = 0;
     paragraph.html("");
   }
   if (vnt && counter < phrase.length && frameCount%int(random(3, 6)) == 0){
@@ -20,7 +28,7 @@ function draw(){
     counter++;
   }
   if (counter >= phrase.length && frameCount%100 == 0) vnt = false;
-  follow.position(mouseX + 50,mouseY + 50);
+  if (move) follow.position(mouseX + 20, mouseY + 60);
   background(255);
   fill(230);
   ellipse(50, 50, 50, 50);
