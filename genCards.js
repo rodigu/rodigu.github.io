0508c5d5c396bcfa4ff8e8595940060536625d95
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2023-02-21 18:56:54 -0600

    updated website

---
+++ genCards.js
@@ -0,0 +1,43 @@
+generateCard(document.getElementById("work"));
+
+generateCard(document.getElementById("games"), {
+  hrefLink: "https://rmorais.itch.io/xadado",
+  title: "Xadado",
+  description:
+    "A dice board game for two. Made with the TIC-80 retro fantasy console.",
+  languageLogo:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Lua-Logo.svg/1200px-Lua-Logo.svg.png",
+  topImage: "./imgs/dademomenu.gif",
+  bottomImage: "./imgs/dademoplay.gif",
+});
+
+generateCard(document.getElementById("projects"), {
+  hrefLink: "https://github.com/rodigu/supernova-age",
+  title: "Supernova Age Prediction",
+  description: "Using clustering algorithms to predict the age of supernovae.",
+  languageLogo:
+    "https://cdn4.iconfinder.com/data/icons/scripting-and-programming-languages/512/Python_logo-512.png",
+  topImage: "./imgs/littlestar.png",
+  bottomImage: "./imgs/supernovass.png",
+});
+
+generateCard(document.getElementById("projects"), {
+  hrefLink: "https://github.com/Liu-Hy/Food-Bank-Network-Simulation",
+  title: "Foodbank Simulation",
+  description: "Monte Carlo simulation of food distribution by foodbanks.",
+  languageLogo:
+    "https://cdn4.iconfinder.com/data/icons/scripting-and-programming-languages/512/Python_logo-512.png",
+  topImage: "./imgs/foodcover.png",
+  bottomImage: "./imgs/foodbanksimcode.png",
+});
+
+generateCard(document.getElementById("projects"), {
+  hrefLink: "https://github.com/Liu-Hy/Food-Bank-Network-Simulation",
+  title: "WFC",
+  description:
+    "Wave function collapse algorithm implemented inside a TIC-80 retro fantasy console.",
+  languageLogo:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Lua-Logo.svg/1200px-Lua-Logo.svg.png",
+  topImage: "./imgs/wfcgifcover.gif",
+  bottomImage: "./imgs/wfccover.png",
+});
--- script.js
+++ script.js
@@ -3,21 +3,16 @@ const generateCard = (
   args = {
     hrefLink: "https://deno.land/x/nets",
     title: "NeTS",
-    description: "Deno library for Network Science built in Typescript",
+    description:
+      "Network Science library. Currently used for research on trade cycles in currency markets.",
     languageLogo:
       "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
     topImage: "./imgs/denologo.png",
     bottomImage: "./imgs/netscode.png",
   }
 ) => {
-  const {
-    hrefLink,
-    title,
-    description,
-    languageLogo,
-    topImage,
-    bottomImage,
-  } = args;
+  const { hrefLink, title, description, languageLogo, topImage, bottomImage } =
+    args;
   const newCard = document.createElement("div");
 
   newCard.classList.add("card");
@@ -42,4 +37,42 @@ const generateCard = (
   parentElement.appendChild(newCard);
 };
 
-generateCard(document.body);
+let leftButton = document.getElementById("leftbutton");
+let rightButton = document.getElementById("rightbutton");
+let centralButton = document.getElementById("centralbutton");
+
+let gamesSection = document.getElementById("games");
+let workSection = document.getElementById("work");
+let projectsSection = document.getElementById("projects");
+
+let sectionOrder = new Map();
+sectionOrder.set("projects", projectsSection);
+sectionOrder.set("work", workSection);
+sectionOrder.set("games", gamesSection);
+
+function changeSection(buttonChosen) {
+  let sectionKeys = [...sectionOrder.keys()];
+
+  let [left, center, right] = sectionKeys;
+
+  console.log(buttonChosen);
+
+  if (buttonChosen === left) {
+    direction = "left";
+  } else if (buttonChosen === right) {
+    direction = right;
+  }
+
+  let menuOrder = ["left", "center", "right"];
+
+  for (idx in sectionKeys) {
+    key = sectionKeys[idx];
+    sectionOrder.get(key).dataset.status = menuOrder[idx];
+  }
+
+  if (buttonChosen === "projects")
+    sectionOrder.get("work").dataset.status = "right";
+  else sectionOrder.get("work").dataset.status = "left";
+
+  sectionOrder.get(buttonChosen).dataset.status = "center";
+}
